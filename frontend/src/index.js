8a767dd33e10971038526351c97fc74d0507804c
Teo Economu <economuteo@gmail.com>
2024-07-30 01:32:33 +0300

    small changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./assets/css/index.css";
-import App from "./App";
+import App from "./App.jsx";
 import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "@mui/material/styles";
 import { ToastContainer } from "react-toastify";
