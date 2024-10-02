8d5c2198627c37fd0d8d6b8e9c7f2d86c98dd589
Economu Teo-Antonio <teoantonioeconomu@gmail.com>
2024-10-02 08:56:33 +0300

    WIP

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,12 +9,14 @@ import theme from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
+    // <React.StrictMode>
+    <>
         <ToastContainer className="toastContainer" />
         <ThemeProvider theme={theme}>
             <App />
         </ThemeProvider>
-    </React.StrictMode>
+    </>
+    // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
