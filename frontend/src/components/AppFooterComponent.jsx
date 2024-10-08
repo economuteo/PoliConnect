ce5e4c112c94a9ebd9c9bafef2790027691d411e
Economu Teo-Antonio <teoantonioeconomu@gmail.com>
2024-10-08 18:38:58 +0300

    WIP

--- frontend/src/components/AppFooterComponent.jsx
+++ frontend/src/components/AppFooterComponent.jsx
@@ -1,25 +1,43 @@
-import { NavLink } from "react-router-dom";
-
+import { useState, useEffect } from "react";
+import { NavLink, useNavigate, useLocation } from "react-router-dom";
 import { HouseIcon, MessagesIcon, NotificationsIcon, ProfileIcon } from "../components";
-
 import { ReactComponent as PlusIcon } from "../assets/images/plus-icon.svg";
-
 import Wrapper from "../assets/wrappers/AppFooterComponent";
 
 const AppFooterComponent = () => {
+    const [isActive, setIsActive] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const handleClick = (path) => {
+        if (!loading) {
+            setLoading(true);
+            navigate(path);
+        }
+    };
+
+    useEffect(() => {
+        setLoading(false);
+    }, [location.pathname]);
+
     return (
         <Wrapper>
-            <NavLink to="/feed">{({ isActive }) => <HouseIcon isActive={isActive} />}</NavLink>
-            <NavLink to="/messages">
-                {({ isActive }) => <MessagesIcon isActive={isActive} />}
-            </NavLink>
-            <NavLink to="/addPost">
+            <div onClick={() => handleClick("/feed")} disabled={loading}>
+                <HouseIcon isActive={isActive} />
+            </div>
+            <div onClick={() => handleClick("/messages")} disabled={loading}>
+                <MessagesIcon isActive={isActive} />
+            </div>
+            <div onClick={() => handleClick("/addPost")} disabled={loading}>
                 <PlusIcon />
-            </NavLink>
-            <NavLink to="/notifications">
+            </div>
+            <div onClick={() => handleClick("/notifications")} disabled={loading}>
                 {({ isActive }) => <NotificationsIcon isActive={isActive} />}
-            </NavLink>
-            <NavLink to="/profile">{({ isActive }) => <ProfileIcon isActive={isActive} />}</NavLink>
+            </div>
+            <div onClick={() => handleClick("/profile")} disabled={loading}>
+                {({ isActive }) => <ProfileIcon isActive={isActive} />}
+            </div>
         </Wrapper>
     );
 };
