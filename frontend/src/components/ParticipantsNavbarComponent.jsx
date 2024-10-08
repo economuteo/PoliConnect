ce9731cc63c8a3fc4b6f1f95152867863c1fd39b
Economu Teo-Antonio <teoantonioeconomu@gmail.com>
2024-10-08 13:14:57 +0300

    WIP

--- frontend/src/components/ParticipantsNavbarComponent.jsx
+++ frontend/src/components/ParticipantsNavbarComponent.jsx
@@ -1,13 +1,18 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 import SearchUsersBackIcon from "../assets/images/search-users-back-icon.png";
 
 import Wrapper from "../assets/wrappers/LikesNavbarComponent";
 
 const ParticipantsNavbarComponent = () => {
+    const [isNavigating, setIsNavigating] = useState(false);
     const navigate = useNavigate();
 
     const handleBackClick = () => {
+        if (isNavigating) return;
+        setIsNavigating(true);
+
         navigate(-1);
     };
 
