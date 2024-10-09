ecc5094b337ccb48180614c3bc5ed60eae6f66c5
Economu Teo-Antonio <teoantonioeconomu@gmail.com>
2024-10-09 13:37:00 +0300

    WIP

--- frontend/src/pages/AccountCreatedPage.jsx
+++ frontend/src/pages/AccountCreatedPage.jsx
@@ -1,15 +1,13 @@
-import { Link, useNavigate } from "react-router-dom";
-
+import { useState, useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Wrapper from "../assets/wrappers/AccountCreatedPage";
-
 import SuccessIcon from "../assets/images/success-icon.png";
-import { useContext, useEffect } from "react";
 import { AppContext } from "../contexts/AppContext";
 
 const CongratsPage = () => {
     const navigate = useNavigate();
-
     const { setRemoveNavbar } = useContext(AppContext);
+    const [isNavigating, setIsNavigating] = useState(false);
 
     useEffect(() => {
         setRemoveNavbar(true);
@@ -17,14 +15,21 @@ const CongratsPage = () => {
         return () => setRemoveNavbar(false);
     }, [setRemoveNavbar]);
 
+    const handleClick = () => {
+        if (!isNavigating) {
+            setIsNavigating(true);
+            navigate("/feed");
+        }
+    };
+
     return (
         <Wrapper>
             <img src={SuccessIcon} className="successIcon" alt="Success Icon" />
             <p className="title">Congrats!</p>
-            <p className="shortDescription">Account Created Succesfuly</p>
-            <Link to="/feed">
-                <button>Get Started</button>
-            </Link>
+            <p className="shortDescription">Account Created Successfully</p>
+            <button onClick={handleClick} disabled={isNavigating}>
+                Get Started
+            </button>
         </Wrapper>
     );
 };
