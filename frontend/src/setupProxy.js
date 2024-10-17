aa0344bb0f3dfe3a08fe06702d9cab46ab2abce7
Economu Teo-Antonio <teoantonioeconomu@gmail.com>
2024-10-17 13:17:28 +0300

    WIP

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,10 +1,15 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
+    const target =
+        process.env.NODE_ENV === "production"
+            ? "https://policonnect.onrender.com/api"
+            : "http://localhost:8080/api";
+
     app.use(
         "/api",
         createProxyMiddleware({
-            target: "http://localhost:8080/api",
+            target: target,
             changeOrigin: true,
             pathRewrite: {
                 "^/api": "",
--- frontend/src/utils/customFetch.js
+++ frontend/src/utils/customFetch.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
+const baseURL =
+    process.env.NODE_ENV === "production"
+        ? "https://policonnect.onrender.com/api/v1"
+        : "http://localhost:3000/api/v1";
+
 const customFetch = axios.create({
-    baseURL: "http://localhost:3000/api/v1",
-    // baseURL: "http://localhost:3000/api/v1",
+    baseURL: baseURL,
 });
 
 export default customFetch;
