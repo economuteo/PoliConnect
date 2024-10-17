0401279252fabd6ec03e3b886edb9a2404abe013
Economu Teo-Antonio <teoantonioeconomu@gmail.com>
2024-10-17 16:34:28 +0300

    WIP

--- frontend/src/utils/customFetch.js
+++ frontend/src/utils/customFetch.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const baseURL =
-    process.env.NODE_ENV === "production"
-        ? "https://policonnect.onrender.com/api/v1"
-        : "http://localhost:3000/api/v1";
+const baseURL = process.env.NODE_ENV === "production" ? "/api/v1" : "http://localhost:3000/api/v1";
 
 const customFetch = axios.create({
     baseURL: baseURL,
